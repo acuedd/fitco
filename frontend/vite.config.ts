d5784d5e7546c7536e318804bf6ef934352e8865
acuedd <acued89@gmail.com>
2025-06-20 10:11:29 -0600

    fix: fixing deployment for frontend with docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'cypress/'],
+      extension: ['.js', '.ts', '.jsx', '.tsx'],
+      cypress: true,
+    })],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+});
