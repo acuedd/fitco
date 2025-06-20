36b95e6a8d894b48729b30c68d1b7b58a4ae66ce
acuedd <acued89@gmail.com>
2025-06-20 07:17:59 -0600

    feat: adding swagger support to apis

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(json({ limit: '10mb' }));
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('DevTalk API')
+    .setDescription('API para carga, gestión de comunicación de devTalk')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
